faf36dbb12ea5d9b74f24dc79bff8b6c04c74538
buvoli <buvoli@uw.edu>
2020-08-05 00:20:17 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # fedora-sc
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://github.com/gitbucket/gitbucket/blob/master/LICENSE)
+
 A prebuilt cjr stack for scientific computing that is based on Fedora 32.
+
+## Description
+
+This stack provides basic support for the following packages:
+
+1. **Languages**
+   - Python 3
+   - c, c++
+   - Fortran
+   - Julia
+   - R
+   - Latex
+2. **Libraries**
+   - Matplotlib
+   - BLAS, LAPACK
+   - Open MPI
+   - X11
+3. **Dev Environments**
+   - Jupyter notebook, Jupyter lab
+   - Theia
+   - vim, git, vim, emacs, tmux
+
+The configurations for Jupyter and Theia are respectively stored the directories `config/jupyter` and `config/theia` which are bound to `~/.jupyter` and `~/.theia` in the container.
+The stack comes preconfigured with theia extensions for python, c/c++ and Fortran.
+
+When building, the image modifies the default container user so that id and group id match with the host.
+
+## Installation
+
+To use use this stack with cjr simply run the command
+```console
+cjr stack:pull https://github.com/container-job-runner/fedora-sc.git
+```
+You can then build the stack by running
+```console
+cjr stack:build stack-fedora-basic
+```
---
+++ build/usermod.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+# ==============================================================================
+# A script for changing ID and group ID of the non-root user. It responds to the
+# following environmental variables:
+#
+#     USER_NAME       (required) username for new linux user
+#     USER_ID         (required) ID for new linux user
+#     GROUP_ID        (required) Group ID for new linux user
+#     USER_PASSWORD   (optional) password for new linux user
+#     GRANT_SUDO      (optional) if "TRUE" then user will have sudo privilages
+#
+# Note: Ideally we would simply run the commands:
+#
+#     groupmod -o -g $GROUP_ID $USER_NAME
+#     usermod -u $USER_ID -g $GROUP_ID $USER_NAME
+#     chown -R $USER_NAME:$USER_NAME /home/$USER_NAME
+#
+# However, problems will occur if ID is large. See for example:
+# - https://github.com/moby/moby/issues/5419 (comments about usermod)
+# - https://github.com/moby/moby/issues/5419
+# - https://github.com/jupyter/docker-stacks/issues/923
+# Instead we remove the old user, create a new one, and chown the home directory
+# ==============================================================================
+
+# -- exit if $USER variables are not set ---------------------------------------
+if [ -z "$USER_NAME" ] || [ -z "$USER_ID" ]  || [ -z "$GROUP_ID" ] ; then
+  exit
+fi
+
+CUR_ID=$(id -u $USER_NAME)
+CUR_GID=$(id -g $USER_NAME)
+
+# -- modify user ---------------------------------------------------------------
+if [ "$CUR_ID" != "$USER_ID" ] || [  "$CUR_GID" != "$GROUP_ID" ] ; then
+  userdel $USER_NAME && rm -r /var/mail/$USER_NAME
+  groupadd -o --gid $GROUP_ID $USER_NAME
+  useradd -M --home /home/$USER_NAME -l -o -s /bin/bash --uid $USER_ID --gid $GROUP_ID $USER_NAME
+  chown -R $USER_NAME:$USER_NAME /home/$USER_NAME # note: this step will be slow if there are many files in home directory
+
+  # -- Set User Password -------------------------------------------------------
+  if [ -n "$USER_PASSWORD" ] ; then
+      echo -e "$USER_PASSWORD\n$USER_PASSWORD" | passwd $USER_NAME
+  fi
+
+  # -- Grant sudo --------------------------------------------------------------
+  if [ "$GRANT_SUDO" = "TRUE" ] ; then
+      if [ -n "$USER_PASSWORD" ] ; then
+          (usermod -aG wheel $USER_NAME)
+      else
+          usermod -aG wheelnopw $USER_NAME
+      fi
+  fi
+
+fi
